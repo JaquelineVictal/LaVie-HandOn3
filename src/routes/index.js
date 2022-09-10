c238f6964025059cd034f40d52b80b20d88ae283
Hellimateas Chaves <91029460+Hellimateas@users.noreply.github.com>
2022-09-10 15:27:05 -0300

    add rotas que serão necessárias para o trabalho

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,20 @@ const express = require("express");
 const produtoController = require("../controllers/produtoController");
 const routes = express.Router();
 
-routes.get("/dashboard/lista", );
-routes.post("/dashboard/criar", );
-routes.delete("/dashboard/:id/deletar", );
-routes.put("/dashboard/:id/atualizar", );
+routes.get("/dashboard/psicologos", );
+routes.get("/dashboard/psicologos/:id")
+routes.post("/dashboard/psicologos", );
+routes.delete("/dashboard/psicologos/:id", );
+routes.put("/dashboard/psicologos/:id", );
+
+routes.get("/dashboard/pacientes", );
+routes.get("/dashboard/pacientes/:id")
+routes.post("/dashboard/pacientes", );
+routes.delete("/dashboard/pacientes/:id", );
+routes.put("/dashboard/pacientes/:id", );
+
+routes.get("/dashboard/atendimentos", );
+routes.get("/dashboard/atendimentos/:id")
+routes.post("/dashboard/atendimentos", );
 
 module.exports = routes;
