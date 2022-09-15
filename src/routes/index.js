b24c3f5f5add8706533f72eeb1ebf66a4605e1f1
JaquelineVictal <78887686+JaquelineVictal@users.noreply.github.com>
2022-09-15 19:13:09 -0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,23 +5,23 @@ const pacientesController = require("../controller/pacientesController");
 const psicologosController = require("../controller/psicologosController");
 const routes = express.Router();
 
-routes.get("/dashboard/login", psicologosController.login);
+routes.get("/login", psicologosController.login);
 
-routes.get("/dashboard/atendimentos", atendimentosController.listarAtendimento);
-routes.get("/dashboard/atendimentos/:id", atendimentosController.listarUmAtendimento);
+routes.get("/atendimentos", atendimentosController.listarAtendimento);
+routes.get("/atendimentos/:id", atendimentosController.listarUmAtendimento);
 routes.post("/dashboard/atendimentos/cadastrar", atendimentosController.cadastrarAtendimento);
 
-routes.get("/dashboard/psicologos", psicologosController.listAll);
-routes.get("/dashboard/psicologos/:id_Psicologos", psicologosController.getOne);
-routes.post("/dashboard/psicologos/cadastrar", psicologosController.cadastrarPsicologo);
-routes.put("/dashboard/psicologos/:id_Psicologos", psicologosController.updatePsicologo);  //Precisa criar a rota
-routes.delete("/dashboard/psicologos/:id_Psicologos", psicologosController.deletarPsicologo);
+routes.get("/psicologos", psicologosController.listAll);
+routes.get("/psicologos/:id_Psicologos", psicologosController.getOne);
+routes.post("/psicologos/cadastrar", psicologosController.cadastrarPsicologo);
+routes.put("/psicologos/:id_Psicologos", psicologosController.updatePsicologo);  //Precisa criar a rota
+routes.delete("/psicologos/:id_Psicologos", psicologosController.deletarPsicologo);
 
-routes.get("/dashboard/pacientes", pacientesController.listarPaciente);
-routes.get("/dashboard/pacientes/:id", pacientesController.listarUmPaciente);
-routes.post("/dashboard/pacientes", pacientesController.cadastrarPaciente);
-routes.delete("/dashboard/pacientes/:id", pacientesController.deletarPaciente); 
-routes.put("/dashboard/pacientes/:id", pacientesController.listarPaciente);  //Precisa criar a rota
+routes.get("/pacientes", pacientesController.listarPaciente);
+routes.get("/pacientes/:id", pacientesController.listarUmPaciente);
+routes.post("/pacientes", pacientesController.cadastrarPaciente);
+routes.delete("/pacientes/:id", pacientesController.deletarPaciente); 
+routes.put("/pacientes/:id", pacientesController.listarPaciente);  //Precisa criar a rota
 
 routes.get("/dashboard/numero-pacientes", dashboardController.contarPacientes);
 routes.get("/dashboard/numero-atendimentos", dashboardController.contarAtendimentos);
