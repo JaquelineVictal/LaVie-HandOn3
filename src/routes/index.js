b12de872a950e4f8b80231bc26bf90ce404771a7
thisfreire <thais.freiire@gmail.com>
2022-09-14 21:48:03 -0300

    Update index.js
    .

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,21 +6,20 @@ const routes = express.Router();
 
 routes.get("/dashboard/login", psicologosController.login);
 
+routes.get("/dashboard/atendimentos", atendimentosController.listarAtendimento);
+routes.get("/dashboard/atendimentos/:id", atendimentosController.listarAtendimento);
+routes.post("/dashboard/atendimentos/cadastrar", atendimentosController.cadastrarAtendimento);
+
 routes.get("/dashboard/psicologos", psicologosController.listAll);
 routes.get("/dashboard/psicologos/:id_Psicologos", psicologosController.getOne);
 routes.post("/dashboard/psicologos/cadastrar", psicologosController.cadastrarPsicologo);
 routes.put("/dashboard/psicologos/:id_Psicologos", psicologosController.updatePsicologo);  //Precisa criar a rota
 routes.delete("/dashboard/psicologos/:id_Psicologos", psicologosController.deletarPsicologo);
 
--routes.get("/dashboard/pacientes", pacientesController.listarPaciente);
+routes.get("/dashboard/pacientes", pacientesController.listarPaciente);
 routes.get("/dashboard/pacientes/:id", pacientesController.listarPaciente);
 routes.post("/dashboard/pacientes", pacientesController.cadastrarPaciente);
 routes.delete("/dashboard/pacientes/:id", pacientesController.listarPaciente);  //Precisa criar a rota
 routes.put("/dashboard/pacientes/:id", pacientesController.listarPaciente);  //Precisa criar a rota
-
-
-routes.get("/dashboard/atendimentos", atendimentosController.listarAtendimento);
-routes.get("/dashboard/atendimentos/:id", atendimentosController.listarAtendimento);
-routes.post("/dashboard/atendimentos/cadastrar", atendimentosController.cadastrarAtendimento);
 
 module.exports = routes;
