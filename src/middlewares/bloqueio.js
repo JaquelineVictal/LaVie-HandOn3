0ae39f0cc119777f53e13d5184bf38d5e6109892
thisfreire <thais.freiire@gmail.com>
2022-09-14 22:06:57 -0300

    Update login
    Inclusão do bloqueio

--- src/middlewares/bloqueio.js
+++ src/middlewares/bloqueio.js
@@ -1,8 +1,16 @@
 module.exports = (req, res, next) => {
     const { pass } = req.query
     if (!pass || pass !== "banana") {
+<<<<<<< Updated upstream
         return res..status(400).json("Senha errada!")
     }
 
     next();
 };
+=======
+        return res.status(400).json("Senha invalida!");
+    }
+
+    next();
+}
+>>>>>>> Stashed changes
--- src/middlewares/requestLog.js
+++ src/middlewares/requestLog.js
@@ -1,3 +1,8 @@
 module.exports = (req, res, next) => {
+<<<<<<< Updated upstream
     console.log('O ip ${req.ip} acessou a rota: ${req.originalUrl}');
+=======
+    console.log('O ip: ${req.ip} acessou a rota: ${req.orginalUrl}');
+    next();
+>>>>>>> Stashed changes
 };
