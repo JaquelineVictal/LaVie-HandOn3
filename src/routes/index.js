d92d9b9162d9438cdfd7f38e613c10d5099fea7d
thisfreire <thais.freiire@gmail.com>
2022-09-14 22:09:26 -0300

    Update
    Cópia da main

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const atendimentosController = require("../controller/atendimentosController");
 const pacientesController = require("../controller/pacientesController");
 const psicologosController = require("../controller/psicologosController");
-<<<<<<< Updated upstream
 const usuariosController = require ("../controller/usuariosController");
 const requestLog = require("../middlewares/requestLog")
 const bloqueio = require("../middlewares/bloqueio");
@@ -20,29 +19,6 @@ routes.get("/dashboard/pacientes/:id", pacientesController.listarPaciente);
 routes.post("/dashboard/pacientes/cadastrar", pacientesController.cadastrarPaciente);
 routes.delete("/dashboard/pacientes/:id", pacientesController.listarPaciente);  //Precisa criar a rota
 routes.put("/dashboard/pacientes/:id", pacientesController.listarPaciente);  //Precisa criar a rota
-=======
-const requestLog = require("../middlewares/requestLog");
-const bloqueio = require("../middlewares/bloqueio");
-const routes = express.Router();
-
-routes.get("/dashboard/login", requestLog, bloqueio, psicologosController.login);
-
-routes.get("/dashboard/atendimentos", requestLog, atendimentosController.listarAtendimento);
-routes.get("/dashboard/atendimentos/:id", requestLog, atendimentosController.listarAtendimento);
-routes.post("/dashboard/atendimentos/cadastrar", requestLog, atendimentosController.cadastrarAtendimento);
-
-routes.get("/dashboard/psicologos", requestLog, psicologosController.listAll);
-routes.get("/dashboard/psicologos/:id_Psicologos", requestLog, psicologosController.getOne);
-routes.post("/dashboard/psicologos/cadastrar", requestLog, psicologosController.cadastrarPsicologo);
-routes.put("/dashboard/psicologos/:id_Psicologos", requestLog, psicologosController.updatePsicologo);  //Precisa criar a rota
-routes.delete("/dashboard/psicologos/:id_Psicologos", requestLog, psicologosController.deletarPsicologo);
-
-routes.get("/dashboard/pacientes", requestLog, pacientesController.listarPaciente);
-routes.get("/dashboard/pacientes/:id", requestLog, pacientesController.listarPaciente);
-routes.post("/dashboard/pacientes", requestLog, pacientesController.cadastrarPaciente);
-routes.delete("/dashboard/pacientes/:id", requestLog, pacientesController.listarPaciente);  //Precisa criar a rota
-routes.put("/dashboard/pacientes/:id", requestLog, pacientesController.listarPaciente);  //Precisa criar a rota
->>>>>>> Stashed changes
 
 routes.get("/dashboard/atendimentos", atendimentosController.listarAtendimento);
 routes.get("/dashboard/atendimentos/:id", atendimentosController.listarAtendimento);
