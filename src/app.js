f94acb0983fc459d5ddad06e1b587fea81e49123
thisfreire <thais.freiire@gmail.com>
2022-09-14 21:33:02 -0300

    atualização
    Criação de usuário.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const routes = require("./routes");
+const requestLog = require("./middlewares/requestLog");
 const handleError = require("./middlewares/handleError");
-
 const db = require("./database");
 const { request } = require("express");
 
@@ -10,7 +10,7 @@ const app = express();
 db.hasConection();
 
 app.use(express.json());
-
+app.use(requestLog);
 app.use(routes);
 app.use(handleError);
 
---
+++ src/middlewares/requestLog.js
@@ -0,0 +1,3 @@
+module.exports = (req, res, next) => {
+    console.log('O ip ${req.ip} acessou a rota: ${req.originalUrl}');
+};
