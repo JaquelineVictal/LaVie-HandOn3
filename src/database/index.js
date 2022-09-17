7d4fe389f850f9cca8d0179eff14ce29c5738366
JaquelineVictal <78887686+JaquelineVictal@users.noreply.github.com>
2022-09-16 22:45:02 -0300

    Update index.js

--- src/database/index.js
+++ src/database/index.js
@@ -2,7 +2,7 @@ const Sequelize = require("sequelize");
 
 const DB_NAME = "LaVie";
 const DB_USER = "root";
-const DB_PASS = "$_Jaque1206_$";
+const DB_PASS = "";
 const DB_CONFIG = { 
     dialect: 'mysql', 
     host: 'localhost', 
